31c0cf4676a3f88b2b9823ec4889a7246837cda0
suzukiken <suzukiken@gmail.com>
2021-03-18 13:30:48 +0000

    done

--- lib/cdkamplify-stack.ts
+++ lib/cdkamplify-stack.ts
@@ -1,9 +1,41 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as codecommit from "@aws-cdk/aws-codecommit";
+import * as amplify from "@aws-cdk/aws-amplify";
 
 export class CdkamplifyStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id);
 
-    // The code that defines your stack goes here
+    const DOMAIN_NAME = 'figmentresearch.com'
+    const PREFIX_NAME = id.toLowerCase().replace('stack', '')
+
+    const repo = new codecommit.Repository(this, "repo", {
+      repositoryName: PREFIX_NAME + '-repo',
+    });
+
+    const app = new amplify.App(this, "cdkamp_ampapp", {
+      appName: PREFIX_NAME + '-app',
+      sourceCodeProvider: new amplify.CodeCommitSourceCodeProvider({
+        repository: repo,
+      }),
+      environmentVariables: {
+        SSM_PARAMETER_NAME: `${PREFIX_NAME}-exports-json`,
+      }
+    });
+
+    const masterBranch = app.addBranch("master");
+
+    const domain = new amplify.Domain(this, "domain", {
+      app: app,
+      domainName: DOMAIN_NAME,
+      subDomains: [
+        {
+          branch: masterBranch,
+          prefix: PREFIX_NAME,
+        },
+      ],
+    });
+
+    app.addCustomRule(amplify.CustomRule.SINGLE_PAGE_APPLICATION_REDIRECT);
   }
 }
