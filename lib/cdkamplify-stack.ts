48b18800c62ad3feb320c97f03fa6533db76985f
suzukiken <suzukiken@gmail.com>
2021-03-18 22:55:40 +0000

    done

--- lib/cdkamplify-stack.ts
+++ lib/cdkamplify-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from "@aws-cdk/core";
 import * as codecommit from "@aws-cdk/aws-codecommit";
 import * as amplify from "@aws-cdk/aws-amplify";
 import * as iam from "@aws-cdk/aws-iam";
+import * as ssm from "@aws-cdk/aws-ssm";
 
 export class CdkamplifyStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -9,34 +10,25 @@ export class CdkamplifyStack extends cdk.Stack {
 
     const DOMAIN_NAME = 'figmentresearch.com'
     const PREFIX_NAME = id.toLowerCase().replace('stack', '')
+    const SSM_PARAM_NAME = PREFIX_NAME + '-exports-json'
 
     const repo = new codecommit.Repository(this, "repo", {
       repositoryName: PREFIX_NAME + '-repo',
     });
     
-    const role = new iam.Role(this, "role", {
-      assumedBy: new iam.ServicePrincipal("amplify.amazonaws.com"),
-      roleName: PREFIX_NAME + '-role',
-    });
+    const param = ssm.StringParameter.fromStringParameterName(this, 'param', SSM_PARAM_NAME)
     
-    role.addManagedPolicy(
-      iam.ManagedPolicy.fromAwsManagedPolicyName("AdministratorAccess-Amplify")
-    );
-
-    role.addManagedPolicy(
-      iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonSSMReadOnlyAccess")
-    );
-
     const app = new amplify.App(this, "cdkamp_ampapp", {
       appName: PREFIX_NAME + '-app',
       sourceCodeProvider: new amplify.CodeCommitSourceCodeProvider({
         repository: repo,
       }),
       environmentVariables: {
-        SSM_PARAMETER_NAME: `${PREFIX_NAME}-exports-json`,
-      },
-      // role: role
+        SSM_PARAMETER_NAME: SSM_PARAM_NAME,
+      }
     });
+    
+    param.grantRead(app);
     
     const masterBranch = app.addBranch("master");
 
