73045e276affa6bcd53d479118e73cf623a5acac
suzukiken <suzukiken@gmail.com>
2021-03-18 21:53:35 +0000

    role

--- lib/cdkamplify-stack.ts
+++ lib/cdkamplify-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from "@aws-cdk/core";
 import * as codecommit from "@aws-cdk/aws-codecommit";
 import * as amplify from "@aws-cdk/aws-amplify";
+import * as iam from "@aws-cdk/aws-iam";
 
 export class CdkamplifyStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -12,6 +13,19 @@ export class CdkamplifyStack extends cdk.Stack {
     const repo = new codecommit.Repository(this, "repo", {
       repositoryName: PREFIX_NAME + '-repo',
     });
+    
+    const role = new iam.Role(this, "role", {
+      assumedBy: new iam.ServicePrincipal("amplify.amazonaws.com"),
+      roleName: PREFIX_NAME + '-role',
+    });
+    
+    role.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName("AdministratorAccess-Amplify")
+    );
+
+    role.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonSSMReadOnlyAccess")
+    );
 
     const app = new amplify.App(this, "cdkamp_ampapp", {
       appName: PREFIX_NAME + '-app',
@@ -20,9 +34,10 @@ export class CdkamplifyStack extends cdk.Stack {
       }),
       environmentVariables: {
         SSM_PARAMETER_NAME: `${PREFIX_NAME}-exports-json`,
-      }
+      },
+      role: role
     });
-
+    
     const masterBranch = app.addBranch("master");
 
     const domain = new amplify.Domain(this, "domain", {
